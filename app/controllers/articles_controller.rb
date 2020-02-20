f819c9d3e2e7ebfb37439951af0b5b2c0962b5f1
alex james <apjames93@gmail.com>
2020-02-20 15:44:07 -0700

    edit and update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit 
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
 
@@ -12,6 +16,16 @@ class ArticlesController < ApplicationController
       redirect_to article_path(@article)
     else
       render 'new'
+    end
+  end
+
+  def update
+     @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = 'Article was successfully updated'
+      redirect_to article_path(@article)
+    else
+      render 'edit'
     end
   end
 
