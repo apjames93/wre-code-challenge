4310d79a95e388d9339c55c8945a189d958100a9
alex james <apjames93@gmail.com>
2020-02-20 19:34:20 -0700

    before_action set_artical

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,16 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
   def new
     @article = Article.new
   end
 
   def edit 
-    @article = Article.find(params[:id])
   end
 
   def create
     @article = Article.new(article_params)
-
     if @article.save
       flash[:notice] = 'Article was successfully created'
       redirect_to article_path(@article)
@@ -20,7 +20,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-     @article = Article.find(params[:id])
+
     if @article.update(article_params)
       flash[:notice] = 'Article was successfully updated'
       redirect_to article_path(@article)
@@ -30,7 +30,6 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -38,13 +37,16 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = 'Article was successfully deleted'
     redirect_to articles_path
   end
 
   private
+
+  def set_article
+    @article = Article.find(params[:id]) 
+  end
 
   def article_params
     params.require(:article).permit(:title, :description)
