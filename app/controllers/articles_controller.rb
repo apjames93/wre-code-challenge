990fc1b9863058a56a9f9ff177818d6cee8b7633
alex james <apjames93@gmail.com>
2020-02-25 10:31:13 -0700

    added in user info to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
       flash[:notice] = 'Article was successfully created'
       redirect_to article_path(@article)
@@ -20,7 +21,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-
     if @article.update(article_params)
       flash[:notice] = 'Article was successfully updated'
       redirect_to article_path(@article)
