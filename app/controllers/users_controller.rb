e640f56b320563073b4e2d73444f99a9cdfe69fb
alex james <apjames93@gmail.com>
2020-02-25 11:19:05 -0700

    set up edit and create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,21 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id]) 
+  end
+
+  def update
+    @user = User.find(params[:id]) 
+
+    if @user.update(user_params)
+      flash[:success] = 'Account updated'
+    else
+      render 'edit'
+    end
+
+  end
+
   private
 
   def user_params
