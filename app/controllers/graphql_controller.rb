622558aa45c4146d82d84bac720abb3c3129c464
alex james <apjames93@gmail.com>
2020-02-26 16:42:52 -0700

    this might change bug scaffold gql controller and pass current_user to context, not sure how we will want to assign from a decuppled front end

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -3,9 +3,9 @@ class GraphqlController < ApplicationController
     variables = ensure_hash(params[:variables])
     query = params[:query]
     operation_name = params[:operationName]
+
     context = {
-      # Query context goes here, for example:
-      # current_user: current_user,
+      current_user: current_user,
     }
     result = BlogSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
