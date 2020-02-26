26bd090dc85ed9e52976ece9dcb44e35b4e7af4d
alex james <apjames93@gmail.com>
2020-02-25 18:59:06 -0700

    admin added to user and small refactors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,14 @@
-class User < ApplicationRecord
-  has_many :articles
-
+class User < ActiveRecord::Base
+  has_many :articles, dependent: :destroy
   before_save { self.email = email.downcase }
-
-  validates :username, presence: true,
-            uniqueness: { case_sensitive: false },
+  validates :username, presence: true, 
+            uniqueness: { case_sensitive: false }, 
             length: { minimum: 3, maximum: 25 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  validates :email, presence: true,
+  validates :email, presence: true, length: { maximum: 105 },
             uniqueness: { case_sensitive: false },
-            length: { minimum: 3, maximum: 105 },
             format: { with: VALID_EMAIL_REGEX }
-  
   has_secure_password
-  
 end
