ffb2626fb1171f5bb5d42b3707ab2c36e12348e2
alex james <apjames93@gmail.com>
2020-02-25 19:03:04 -0700

    formatting fix

--- Gemfile
+++ Gemfile
@@ -16,10 +16,13 @@ gem 'bcrypt', '~> 3.1.7'
 gem 'will_paginate', '3.1.7'
 gem 'bootstrap-will_paginate', '1.0.0'
 
+# Use SCSS for stylesheets
+gem 'bootstrap-sass', '~> 3.3.5'
+gem 'sass-rails', '>= 6'
+
+
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
-# Use SCSS for stylesheets
-gem 'sass-rails', '>= 6'
 # Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
 gem 'webpacker', '~> 4.0'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
 class SessionsController < ApplicationController
-
+  
   def new
   end
-
+  
   def create
-  user = User.find_by(email: params[:session][:email].downcase)
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:success] = "You have successfully logged in"
@@ -14,11 +14,11 @@ class SessionsController < ApplicationController
       render 'new'
     end
   end
-
+  
   def destroy
     session[:user_id] = nil
     flash[:success] = "You have logged out"
     redirect_to root_path
   end
-
+  
 end
