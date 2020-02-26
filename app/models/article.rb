25c9bee7b57c99fcb812c039a8a7f60ade016f25
alex james <apjames93@gmail.com>
2020-02-25 18:55:43 -0700

    Article refactor to use and show categorys and relationships

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
-class Article < ApplicationRecord
+class Article < ActiveRecord::Base
   belongs_to :user
-  validates :title, presence: true, length: { minimum: 3, maximun: 50 }
-  validates :description, presence: true, length: { minimum: 10, maximun: 300 }
+  has_many :article_categories
+  has_many :categories, through: :article_categories
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
   validates :user_id, presence: true
 end
