c562bb2292f848cf2750ced9e7fc64cb32cac76f
alex james <apjames93@gmail.com>
2020-02-25 19:03:49 -0700

    forgot to commit routes and commiting schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-
-  root 'page#home'
-  get 'about', to: 'page#about'
-
+  
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  
   resources :articles
-
+  
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
-
+  
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  
+  resources :categories, except: [:destroy]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_174650) do
+ActiveRecord::Schema.define(version: 2020_02_25_232333) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -20,12 +25,19 @@ ActiveRecord::Schema.define(version: 2020_02_25_174650) do
     t.integer "user_id"
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
