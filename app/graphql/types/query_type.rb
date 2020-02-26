4af46263db126abbc352cb4b9f39d33a0f301552
alex james <apjames93@gmail.com>
2020-02-26 11:27:11 -0700

    FieldResolvers wrapping QueryType to include fields in QueryType

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,10 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
-    end
+    include FieldResolvers::ArticleField
+    include FieldResolvers::ArticlesField
+    include FieldResolvers::CategoriesField
+    include FieldResolvers::CategoryField
+    include FieldResolvers::UserField
+    include FieldResolvers::UsersField
   end
 end
