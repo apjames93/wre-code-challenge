20c1dc7194731aef8a95e748a76079ba0f6f2000
alex james <apjames93@gmail.com>
2020-02-26 18:18:48 -0700

    fixing gql for heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //
 //= require jquery
-
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,7 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w(application.js)
+Rails.application.config.assets.precompile += %w(graphiql/rails/application.js graphiql/rails/application.css)
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
