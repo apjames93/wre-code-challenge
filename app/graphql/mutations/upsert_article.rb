f1154a58b7d15b2236b3e07c26c4f395d28aa6f2
Alex James <apjames93@gmail.com>
2020-03-14 13:33:32 -0600

    clean up and error handling on UpsertArticle

--- app/graphql/mutations/upsert_article.rb
+++ app/graphql/mutations/upsert_article.rb
@@ -2,21 +2,30 @@
 
 module Mutations
   class UpsertArticle < Mutations::BaseMutation
+    description 'Creates or updates an article. Pass an ID with an article to update, omit ID to create. Pass category_ids to assign new categories to the article.'
     argument :article, Inputs::ArticleInput, required: true
     argument :category_ids, [ID], required: false
     type Types::ArticleType
 
     def resolve(args)
+      category_ids = args[:category_ids] || []
       args_hash = args[:article].to_h
 
       if args_hash[:id]
         article = Article.find(args_hash[:id])
+        article.update(args_hash)
       else 
         article = Article.new(args_hash)
+        article.save
       end
 
-      article.update(args_hash.merge(user_id: user_id))
-      args[:category_ids].each { |id| ArticleCategory.create(article: article, category_id: id) } if args[:category_ids].count > 0
+      if category_ids.count > 0
+        category_ids.each do |id|
+          ArticleCategory.create(article: article, category_id: id) if ArticleCategory.find_by(article: article, category_id: id).nil?
+        end
+      end
+      
+      raise GraphQL::ExecutionError.new(article.errors.full_messages) unless article.errors.empty?
 
       article
     end
