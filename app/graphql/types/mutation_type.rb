15b211b0ef45bca20ed4a46013bb744fd8a5efec
Alex James <apjames93@gmail.com>
2020-03-14 13:33:52 -0600

    remove destroy from mutations

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,6 +2,5 @@ module Types
   class MutationType < Types::BaseObject
     field :upsert_article, mutation: Mutations::UpsertArticle
     field :upsert_category, mutation: Mutations::UpsertCategory
-    field :destroy_article, mutation: Mutations::DestroyArticle
   end
 end
